5d7764464ed240c379fce15095c16a6e304907fa
KiritoKing <kiritoclzh@gmail.com>
2022-10-13 21:14:23 +0800

    modify entry point

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from concurrent.futures import process
+import string
 from threading import Thread
 from server import start_server, app
 import webview
+import os
 
+def get_entrypoint():
+    def exists(path: string):
+        return os.path.exists(os.path.join(os.path.dirname(__file__), path))
 
+    if exists('./gui/index.html'):
+        return './gui/index.html'
+
+    raise Exception('No index.html found')
 
 if __name__ == '__main__':
-    t = Thread(target=start_server)
-    t.daemon = True
+    t = Thread(target=start_server, daemon=True)
     t.start()
 
     webview.create_window('Video Processor', 
