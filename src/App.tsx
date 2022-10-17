626ed7c3cf2c1ae3d716dab9cea24632154ae922
cardiq <563397455@qq.com>
2022-10-18 01:02:11 +0800

    图片转矩阵的组件初步实现

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from 'react';
 import { Box } from '@mui/material';
 import './Styles/App.css';
 import FileInput from './Components/FileInput/FileInput';
@@ -9,10 +9,12 @@ import AlertBar from './Components/AlertBar/AlertBar';
 
 import { IStatus } from './Types/Props';
 
+import CanvasFrame from './Components/CanvasFrame'
+
 function App() {
   const [file, setFile] = useState<File>();
   const [loading, setLoading] = useState<boolean>(false);
-  const [toastStatus, setToastStatus] = useState<IStatus>({status: 0});
+  const [toastStatus, setToastStatus] = useState<IStatus>({ status: 0 });
   const inputRef = useRef<HTMLInputElement>(null);
 
   const uploadHandler = () => {
@@ -21,22 +23,22 @@ function App() {
     uploadFunc(uploadAddr, file).then(result => {
       setLoading(false);
       setFile(undefined);
-      if(result) {
+      if (result) {
         // window.alert("上传成功");
-        setToastStatus({status: 1});
+        setToastStatus({ status: 1 });
       }
       else {
         // window.alert("上传失败")
-        setToastStatus({status: 1, text:"服务器没有接收到文件"});
+        setToastStatus({ status: 1, text: "服务器没有接收到文件" });
       }
     }).catch(error => {
       setLoading(false);
-      setToastStatus({status: 2, text: "服务器没有开启"});
+      setToastStatus({ status: 2, text: "服务器没有开启" });
       console.log(error);
     });
   }
   const openHandler = () => {
-    if(inputRef.current != null) {
+    if (inputRef.current != null) {
       inputRef.current.click();
     }
   }
@@ -50,10 +52,11 @@ function App() {
       <div className='MainBar'>
         <Box component="form">
           <FileInput action={(file: File) => setFile(file)} ref={inputRef} />
-          <ControlPanel file={file} loading={loading} openHandler={()=>openHandler()} uploadHandler={()=>uploadHandler()} clearHandler={()=>clearHandler()} />
+          <ControlPanel file={file} loading={loading} openHandler={() => openHandler()} uploadHandler={() => uploadHandler()} clearHandler={() => clearHandler()} />
+          <CanvasFrame text="id"></CanvasFrame>
         </Box>
       </div>
-    </div>
+    </div >
   );
 }
 
